648e82d2e6bf8fecd19b798d0c517b33d7e0bd67
unknown <juliokasahara@gmail.com>
2021-07-29 19:46:34 -0300

    18 - Emit

--- src/components/Person/picture.vue
+++ src/components/Person/picture.vue
@@ -1,14 +1,26 @@
 <template>
+	<div>
 	<img :src="imagem" :alt="descricao"/>
+	<MudarImagem @mudar-imagem="trocarImagem"/>
+	</div>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue'
 export default{
 	name: 'Picture',
+	components: {
+		MudarImagem
+	},
 	data(){
 		return{
 			imagem:"/img/Vue.png",
 			descricao: "vuuuuuuuuuuuuuuuuuuuuuuue!!!!!!!!!!"
+		}
+	},
+	methods:{
+		trocarImagem(){
+			this.imagem = "/img/emit.png"
 		}
 	}
 }

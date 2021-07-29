2e7c15eedecbd6084e564c502b1b426f067e450b
unknown <juliokasahara@gmail.com>
2021-07-29 18:52:14 -0300

    15 - Múltiplos eventos

---
+++ src/components/MultEvent.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<button @click="primeiro(variavel,$event),segundo($event),apaga($event)"> ativar N eventos</button>
+		<p>{{ texto }}</p>
+	</div>
+</template>
+
+<script>
+
+export default ({
+	name: 'MultEvent',
+	data() {
+		return {
+			texto: "",
+			variavel: "primeiro"
+		}
+	},
+	methods: {
+		primeiro(variavel,e){
+			this.texto = variavel;
+		},
+		segundo(e){
+			setTimeout(() => this.texto = "segundo", 500)
+		},
+		apaga(e){
+			setTimeout(() => this.texto = "", 1000)
+		}
+	}
+})
+</script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,12 +2,17 @@
 	<div>
 		<h3>Ola Vue Componente</h3>
 		<p>{{ nome }} - {{profisao}}</p>
+		<MultEvent />
 	</div>
 </template>
 
 <script>
+import MultEvent from "./MultEvent.vue"
 export default {
 	name: 'PrimeiroComponente',
+	components: {
+		MultEvent
+	},
 	data() {
 		return {
 			nome:"Julio",

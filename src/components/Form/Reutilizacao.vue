ae538b5f96460b410e68ac46535f394fe63d3941
unknown <juliokasahara@gmail.com>
2021-07-29 19:03:50 -0300

    16 - Reutilização de componentes

---
+++ src/components/Form/Reutilizacao.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<button @click="ativar">Clique aqui</button>
+		<span v-show="flg">Ativado!</span>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: "Reutilizacao",
+	data() {
+		return {
+			flg: false
+		}
+	},
+	methods: {
+		ativar(){
+			this.flg = !this.flg
+		}
+	},
+}
+</script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,15 +3,26 @@
 		<h3>Ola Vue Componente</h3>
 		<p>{{ nome }} - {{profisao}}</p>
 		<MultEvent />
+
+		<hr>
+			<Reutilizacao />
+		<hr>
+			<Reutilizacao />
+		<hr>
+			<Reutilizacao />
+		<hr>
 	</div>
 </template>
 
 <script>
 import MultEvent from "./MultEvent.vue"
+import Reutilizacao from "./Form/Reutilizacao"
+
 export default {
 	name: 'PrimeiroComponente',
 	components: {
-		MultEvent
+		MultEvent,
+		Reutilizacao
 	},
 	data() {
 		return {
